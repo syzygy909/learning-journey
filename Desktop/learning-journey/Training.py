cbfc25c51fce2c99a608c5bc0c2343b07cc01f37
syzygy909 <richmond7361@gmail.com>
2026-08-27 10:11:05 +0800

    list

--- Desktop/learning-journey/Training.py
+++ Desktop/learning-journey/Training.py
@@ -1,1 +1,17 @@
-print("Hello, I am newbie here")
+''' # Train Example 2 for list
+import random
+
+n = int(input("Set of numbers: "))
+
+for x in range (n):
+    red = [i for i in range(1, 34)]
+    blue = [random.randrange(1,17)]
+    B = random.choice(blue)
+    R = list(random.sample(red, 6))
+    R.sort
+    [print(f"\033[31m{x:0>2d}", end = " ") for x in R]
+    print(f"\033[34m{B:0>2d}")
+    print()
+'''
+
+printYeahhh
--- Desktop/learning-journey/temporary train.py
+++ Desktop/learning-journey/temporary train.py
@@ -7,3 +7,18 @@ for x in range(0, 21):
 '''
 
 
+import random
+
+n = int(input("Set of numbers: "))
+
+for x in range (n):
+    red = [i for i in range(1, 34)]
+    blue = [random.randrange(1,17)]
+    B = random.choice(blue)
+    R = list(random.sample(red, 6))
+    R.sort
+    [print(f"\033[31m{x:0>2d}", end = " ") for x in R]
+    print(f"\033[34m{B:0>2d}")
+    print()
+
+
